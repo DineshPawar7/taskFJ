cf6a699b00007c7eea1fbf48177437e657dc3ea2
Dinesh Pawar <dsp70458@gmail.com>
2025-06-23 14:52:03 +0530

    Initialize server with Express framework and basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+
+
+      ek small level mern project banao jisme authentication system dalni hei, and user usme kuch content post bhi karna chahiye jo database me store hona chaiye, and sabhi userso dikhna chaiye, full brif: auth do type ke admin user admin sirf ek hi hoga, users infinite, auth me login register , forget password, enter new password sabhi working pages, login karne ke baad users post upload kar sakte hei, jo bhi users login ya register honge, matlab users ka data admin dashboard me dikhna chaiye, ek admin dashboard banana hei, jisme users ka data dikhega, data like, username password email, admin use update delete kar sakta hei, user ke data ke samne usne post kiya hua content/posts bhi dikhne chahiye admin use bhi update ya delete kkar sakta hei, 
+      
+
+    </div>
+  )g
 }
 
 export default App
